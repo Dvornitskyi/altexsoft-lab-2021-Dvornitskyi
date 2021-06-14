bea4a3b3c4f120e3141413bd25c7b41ad52f2192
Yan Dvornitskyi <dvornitskyi@gmail.com>
2021-06-14 13:14:39 +0300

    second  push

--- homework-2/src/app.js
+++ homework-2/src/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('.plans__slider').slick({
+    $('.plans-slider').slick({
         mobileFirst: true,
         arrows: false,
         dots: false,
@@ -12,7 +12,7 @@ $(document).ready(function(){
         variableHeight: true,
         responsive: [
             {
-                breakpoint: 768,
+                breakpoint: 1024,
                 setting: {
                     slidesToShow: 4,
                     slidesToScroll: 1,
@@ -20,7 +20,7 @@ $(document).ready(function(){
             }
         ],
     });
-    $('.problems__sliders').slick({
+    $('.problems-sliders').slick({
         mobileFirst: true,
         arrows: false,
         dots: true,
@@ -29,44 +29,19 @@ $(document).ready(function(){
         slidesToShow: 1,
         slidesToScroll: 1,
     });
-    $('.three__sliders').slick({
+    $('.client-sliders').slick({
         mobileFirst: true,
         arrows: false,
         autoplay: true,
         autoplaySpeed: 3000,
+        dots: false,
         dots: true,
-        dotsClass: 'slick-dots slider__dots',
+        dotsClass: 'slick-dots slider-dots',
         customPaging: function(slick, index) {
-          let image = $(slick.$slides[index]).find('.three__image').attr('src');
+          let image = $(slick.$slides[index]).find('.client-image').attr('src');
           return '<img src="' + image + '" alt="" /> '
         },
         swipe: false,
         allowTouchMove: false,
     });
 });
-//-----------mobile
-const aroundBody = document.body;
-const headerX = document.querySelector('#headerX');
-const headerMenu = document.querySelector('#headerMenu');
-const headerNav= document.querySelector('#headerNav');
-const headerBtn = document.querySelector('#headerBtn');
-const headerBtnOne = document.querySelector('#headerBtrOne');
-const headerBtnTwo = document.querySelector('#headerBtnTwo');
-headerX.style.display = 'none';
-function mobileMenu() {
-    headerMenu.onclick = () => {
-        headerNav.style.display = 'block';
-        headerBtn.style.display = 'flex';
-        headerX.style.display = 'block';
-        headerMenu.style.display = 'none';
-        aroundBody.style.overflow = 'hidden';
-    }
-    headerX.onclick = () => {
-        headerNav.style.display = 'none';
-        headerBtn.style.display = 'none';
-        headerX.style.display = 'none';
-        headerMenu.style.display = 'block';
-        aroundBody.style.overflow = 'visible';
-    }
-}
-mobileMenu();
